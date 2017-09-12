8517f6e0f63498295ef6709e8a34bb3bac07670c
Seyedamirhossein Hesamian <seyedamirhosse_hesamian@bradycorp.com>
2017-09-12 00:26:16 -0500

    assignment #2

---
+++ src/app/username/username.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrls: ['./username.component.css']
+})
+export class UsernameComponent implements OnInit {
+  username: string;
+
+  constructor() {
+    this.username = "empty username";
+  }
+
+  ngOnInit() {
+  }
+
+  onResetButtonClick() {
+    this.username = "";
+  }
+
+}
